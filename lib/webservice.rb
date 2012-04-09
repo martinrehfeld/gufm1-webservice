ac76a02808b8cd5591463dff08860301537dcb35
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2012-04-09 19:08:02 +0200

    support JSONP

--- lib/webservice.rb
+++ lib/webservice.rb
@@ -3,13 +3,18 @@ require 'gufm'
 
 class Webservice < Sinatra::Application
 
-  before do
-    content_type :json
-  end
-
   get '/' do
     lat, lng, year = params[:lat], params[:lng], params[:year]
-    ActiveSupport::JSON.encode Gufm.new.query(lat, lng, year)
+    callback = params['callback'] # jsonp
+    json = ActiveSupport::JSON.encode Gufm.new.query(lat, lng, year)
+
+    if callback
+      content_type :js
+      "#{callback}(#{json})"
+    else
+      content_type :json
+      json
+    end
   end
 
 end
