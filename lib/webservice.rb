075f9ebf5db7f3ba2cf8cb22f066c64305de7d7d
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2013-11-24 19:47:14 +0100

    add debug output to webservice

--- lib/webservice.rb
+++ lib/webservice.rb
@@ -6,7 +6,9 @@ class Webservice < Sinatra::Application
   get '/' do
     lat, lng, year = params[:lat], params[:lng], params[:year]
     callback = params['callback'] # jsonp
-    json = ActiveSupport::JSON.encode Gufm.new.query(lat, lng, year)
+    result = Gufm.new.query(lat, lng, year)
+    puts "lat: #{lat.inspect}, lng: #{lng.inspect}, year: #{year.inspect} => #{result.output.inspect}"
+    json = ActiveSupport::JSON.encode(result)
 
     if callback
       content_type :js
